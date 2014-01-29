28addbd72dfd7c3edf702ffa7a35470a7b4a47e1
parastuffs <parastuffs@gmail.com>
2014-01-29 18:39:48 +0100

    Footer.

---
+++ html/footer.php
@@ -0,0 +1,3 @@
+<center>
+<p><a href="mailto:parastuffs@gmail.com">contact</a> <a href="https://github.com/parastuffs/facetious-pelican">source</a> <a href="LICENSE.txt">LICENSE</a></p>
+</center>
--- html/index.php
+++ html/index.php
@@ -1,16 +1,13 @@
 <?php
-$html = <<<HTML
-<html>
-	<head>
-		<title>One Facetious Pelican</title>
-	</head>
-	<footer>
-		<center>
-		<p><a href="mailto:parastuffs@gmail.com">contact</a> <a href="https://github.com/parastuffs/facetious-pelican">source</a> <a href="LICENSE.txt">LICENSE</a></p>
-		</center>
-	</footer>
-</html>
-HTML;
+$html = '<html>
+<head>
+	<title>One Facetious Pelican</title>
+</head>
+<footer>';
+echo $html;
+include("footer.php");
+$html = '</footer>
+</html>';
 
 echo $html;
 
