5171d8bff7c4c958bdcd477134bd0a99c150a29a
parastuffs <parastuffs@gmail.com>
2014-01-29 17:30:11 +0100

    index page.

---
+++ html/index.php
@@ -0,0 +1,17 @@
+<?php
+$html = <<<HTML
+<html>
+	<head>
+		<title>One Facetious Pelican</title>
+	</head>
+	<footer>
+		<center>
+		<p><a href="mailto:parastuffs@gmail.com">contact</a> <a href="https://github.com/parastuffs/facetious-pelican">source</a> <a href="LICENSE.txt">LICENSE</a></p>
+		</center>
+	</footer>
+</html>
+HTML;
+
+echo $html;
+
+?>
