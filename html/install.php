92e35b4e31b514d4d55d199f9cc96ec75fc203f4
parastuffs <parastuffs@gmail.com>
2014-02-09 20:31:57 +0100

    Few things.

---
+++ html/install.php
@@ -0,0 +1,32 @@
+<?php
+
+include($_SERVER["DOCUMENT_ROOT"]."/settings.php");
+include($_SERVER["DOCUMENT_ROOT"]."/connectDB.php");
+
+$createStr = 'CREATE TABLE user
+	(id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	password CHAR(64) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id)
+)';
+
+$createStr += 'CREATE TABLE books
+	(id INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL,
+	author VARCHAR(255) NOT NULL,
+	year YEAR,
+	pages INT,
+	publisher VARCHAR(255),
+	isbn13 CHAR(13) NOT NULL UNIQUE,
+	isbn10 CHAR(10) NOT NULL UNIQUE,
+	cover VARCHAR(255),
+	PRIMARY KEY(id)
+)';
+
+$createStr += 'CREATE TABLE book_user
+	(user_id INT NOT NULL,
+	book_id INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE ON UPDATE CASCADE
+)';
---
+++ html/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$db_name = "facetious_pelican_bis";
+$db_host = "localhost";
+$db_user = "";
+$db_passwd = "";
