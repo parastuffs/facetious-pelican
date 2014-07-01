fb9ac7680a28541b841a80037f16efc51890ad71
parastuffs <parastuffs@gmail.com>
2014-07-01 18:28:10 +0200

    Book class.

---
+++ html/Book.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class Book
+{
+	private $_title;
+	private $_id;
+	private $_author;
+	private $_year;
+	private $_pages
+	private $_publisher;
+	private $_isbn13;
+	private $_isbn10;
+	private $_cover;
+	
+	public __construct()
+	{
+		
+	}
+	
+}
