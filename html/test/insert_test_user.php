e67fea6d4ca6a9bfbfe053049b0262070407b67a
parastuffs <parastuffs@gmail.com>
2014-11-12 22:25:07 +0100

    Test user insertion.

---
+++ html/test/insert_test_user.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once($_SERVER["DOCUMENT_ROOT"]."/connectDB.php");
+
+$query = $db->prepare('INSERT INTO users(name, password, email) VALUES(:name, :password, :email)');
+$query->execute(array(
+	'name' => "test_user",
+	'password' => hash("sha256", "test_password"),
+	'email' => "test@mail.com")
+	));
+
+echo 'User inserted. I guess.';
