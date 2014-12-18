f2bf97b05a3f4e6fc98e11ff9d9910a8e4997343
parastuffs <qudelhay@ulb.ac.be>
2014-12-18 01:32:26 +0100

    Update classes.

--- html/Book.class.php
+++ html/Book.class.php
@@ -6,15 +6,30 @@ class Book
 	private $_id;
 	private $_author;
 	private $_year;
-	private $_pages
+	private $_pages;
 	private $_publisher;
 	private $_isbn13;
 	private $_isbn10;
 	private $_cover;
 	
-	public __construct()
+	public function __construct($id, $title, $author, $isbn10, $isbn13)
 	{
-		
+		$this->_id 		= $id;
+		$this->_title 	= $title;
+		$this->_author 	= $author;
+		$this->_isbn10 	= $isbn10;
+		$this->_isbn13 	= $isbn13;
+	}
+
+	public function getTitle()
+	{
+		return $this->_title;
+	}
+
+	public function getAuthor()
+	{
+		return $this->_author;
 	}
 	
 }
+?>
--- html/User.class.php
+++ html/User.class.php
@@ -6,12 +6,15 @@ class User
 	private $_name;
 	private $_email;
 	private $_level;
+	private $_books; //Array
 	
 	public function __construct($email, $name, $id)
 	{
 		$this->_id = $id;
 		$this->_name = $name;
 		$this->_email = $email;
+
+		$this->_books = array();
 	}
 
 	//public function addBook(Book $book)
@@ -37,6 +40,30 @@ class User
 	public function setName($name)
 	{
 		$this->_name = $name;
+	}
+
+	public function getId()
+	{
+		return $this->_id;
+	}
+
+	public function addBook($book)
+	{
+		echo '<br />books init size: '.count($this->_books).'<br />';
+		echo 'Book title to add in user: \''.$book->getTitle().'\'<br />';
+		if(0 == count($this->_books)) {
+			$this->_books[] = $book;
+		}
+		else {
+			array_push($this->_books, $book);
+		}
+		echo '<br />books new isze: '.count($this->_books).'<br />';
+		print_r($this->_books);
+	}
+
+	public function getBooks()
+	{
+		return $this->_books;
 	}
 
 }
